04a16e23545bdcce7ea9487cb91d93ac632f1416
mwcz <mwc@clayto.com>
2019-05-30 13:00:50 -0400

    start chrome loop in a terminal

--- start-chrome.sh
+++ start-chrome.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-if [[ -f /tmp/start-chrome-loop.pid ]]; then
-    kill $(cat /tmp/start-chrome-loop.pid)
-    killall google-chrome
-fi
 
-echo $$ > /tmp/start-chrome-loop.pid
-
-while true; do google-chrome --kiosk http://127.0.0.1:1234/127.0.0.1:8765/; done
+gnome-terminal --working-directory="$HOME/clh-arcade" -e "while true; do google-chrome --kiosk http://127.0.0.1:1234/127.0.0.1:8765/; done"
