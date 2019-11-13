3d5f3169cb2c1c15cfd5f1632232f5fca7b90725
mwcz <mwc@clayto.com>
2019-11-13 14:36:53 -0500

    run bin script from correct path

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+THIS_PATH=$(dirname $(realpath run.sh))
 
-CORSPROXY_MAX_PAYLOAD=40485760 CORSPROXY_HOST=127.0.0.1 CORSPROXY_PORT=1234 ./node_modules/.bin/corsproxy
-./node_modules/.bin/http-server -p 8765
+CORSPROXY_MAX_PAYLOAD=40485760 CORSPROXY_HOST=127.0.0.1 CORSPROXY_PORT=1234 $THIS_PATH/node_modules/.bin/corsproxy
+$THIS_PATH/node_modules/.bin/http-server -p 8765
