28acf82e5224f863010d90ee6c94e1261e2ad74d
mwcz <mwc@clayto.com>
2019-11-13 15:05:27 -0500

    update bin script

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 CORSPROXY_MAX_PAYLOAD=40485760 CORSPROXY_HOST=127.0.0.1 CORSPROXY_PORT=1234 $SCRIPTPATH/../lib/node_modules/clh-arcade/node_modules/.bin/corsproxy &
-$SCRIPTPATH/../lib/node_modules/clh-arcade/node_modules/.bin/http-server -p 8765
+$SCRIPTPATH/../lib/node_modules/clh-arcade/node_modules/.bin/http-server -p 8765 $SCRIPTPATH &
+$SCRIPTPATH/../lib/node_modules/clh-arcade/node_modules/.bin/http-server -p 8766 ./
